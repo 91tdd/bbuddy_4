d4e83ac09bb5a43817a6759e24b7b41149125803
hatelove <jay.hatelove@gmail.com>
2018-02-25 14:31:37 +0800

    refactoring test

--- bbuddy_4/UnitTest1.cs
+++ bbuddy_4/UnitTest1.cs
@@ -6,12 +6,28 @@ namespace bbuddy_4
     [TestClass]
     public class BbuddyTests
     {
+        private Accounting _accounting;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            InitAccounting();
+        }
+
         [TestMethod]
         public void no_budgets()
         {
-            var accounting = new Accounting();
-            var totalBudget = accounting.TotalBudget(new DateTime(2018, 3, 1), new DateTime(2018, 3, 1));
-            Assert.AreEqual(totalBudget, 0);
+            TotalBudgetShouldBe(0, new DateTime(2018, 3, 1), new DateTime(2018, 3, 1));
+        }
+
+        private void TotalBudgetShouldBe(int expected, DateTime startDate, DateTime endDate)
+        {
+            Assert.AreEqual(expected, _accounting.TotalBudget(startDate, endDate));
+        }
+
+        private void InitAccounting()
+        {
+            _accounting = new Accounting();
         }
     }
 }
