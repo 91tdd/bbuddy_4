4b448c99f050511c58f6ba9c164e75167c6f444f
hatelove <jay.hatelove@gmail.com>
2018-02-26 15:55:39 +0800

    refactor: make some public functions to private

--- bbuddy_4/Accounting.cs
+++ bbuddy_4/Accounting.cs
@@ -3,46 +3,6 @@ using System.Linq;
 
 namespace bbuddy_4
 {
-    public class Period
-    {
-        public Period(DateTime startDate, DateTime endDate)
-        {
-            StartDate = startDate;
-            EndDate = endDate;
-        }
-
-        public DateTime StartDate { get; private set; }
-        public DateTime EndDate { get; private set; }
-
-        public DateTime EffectiveEndDate(Budget budget)
-        {
-            var effectiveEndDate = EndDate > budget.LastDay ? budget.LastDay : EndDate;
-            return effectiveEndDate;
-        }
-
-        public DateTime EffectiveStartDate(Budget budget)
-        {
-            return StartDate < budget.StartDay ? budget.StartDay : StartDate;
-        }
-
-        public decimal EffectiveDays(Budget budget)
-        {
-            if (StartDate > budget.LastDay)
-            {
-                return 0;
-            }
-            if (EndDate < budget.StartDay)
-            {
-                return 0;
-            }
-            var effectiveEndDate = EffectiveEndDate(budget);
-            var effectiveStartDate = EffectiveStartDate(budget);
-
-            var days = (effectiveEndDate.AddDays(1) - effectiveStartDate).Days;
-            return days;
-        }
-    }
-
     public class Accounting
     {
         private readonly IBudgetRepo _budgetRepo;
@@ -60,6 +20,46 @@ namespace bbuddy_4
                 return new Period(startDate, endDate).EffectiveDays(budgets[0]);
             }
             return 0;
+        }
+    }
+
+    public class Period
+    {
+        public Period(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public DateTime EndDate { get; private set; }
+        public DateTime StartDate { get; private set; }
+
+        public decimal EffectiveDays(Budget budget)
+        {
+            if (StartDate > budget.LastDay)
+            {
+                return 0;
+            }
+            if (EndDate < budget.StartDay)
+            {
+                return 0;
+            }
+            var effectiveEndDate = EffectiveEndDate(budget);
+            var effectiveStartDate = EffectiveStartDate(budget);
+
+            var days = (effectiveEndDate.AddDays(1) - effectiveStartDate).Days;
+            return days;
+        }
+
+        private DateTime EffectiveEndDate(Budget budget)
+        {
+            var effectiveEndDate = EndDate > budget.LastDay ? budget.LastDay : EndDate;
+            return effectiveEndDate;
+        }
+
+        private DateTime EffectiveStartDate(Budget budget)
+        {
+            return StartDate < budget.StartDay ? budget.StartDay : StartDate;
         }
     }
 }
